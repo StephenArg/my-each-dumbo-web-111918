38653d75b2a407a840cdcb36c22a3c0c73ee8002
Stephen Argentina <stephena9871@gmail.com>
2018-11-10 05:42:32 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,13 +2,9 @@ def my_each(array)
   i = 0
   list = []
   while i < array.length
-    yield(i)
+    yield(array[i])
     list.push(array[i])
     i += 1
   end
   return list
 end
-
-collection = [1, 2, 3, 4]
-
-my_each(collection) {|i| puts i}
